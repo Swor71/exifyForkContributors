972639b8540de03f5cc5cf870f04c4232e0e0c2b
Marcin Swornowski <swor71@gmail.com>
2023-10-02 18:55:24 +0200

    add: aggregate all the entries in a column

--- index.js
+++ index.js
@@ -8,11 +8,20 @@ const apiKey = process.env.NOTION_API_KEY;
 
 const notion = new Client({ auth: apiKey });
 
+const columnName = 'GH Handle';
+
 const querryDatabase = async () => {
   try {
-    const res = await notion.databases.query({ database_id: pageId });
+    const res = await notion.databases.query({
+      database_id: pageId,
+    });
 
-    console.log('RESULT: ', res);
+    console.log(
+      'RESULT: ',
+      res.results.map(
+        (prop) => prop.properties[columnName].rich_text[0].plain_text
+      )
+    );
   } catch (error) {
     console.log('ERROR: ', error);
   }
