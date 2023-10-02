e38eecd05e3d6a3ec60df508d97ad4231a625de0
Marcin Swornowski <swor71@gmail.com>
2023-10-02 20:58:35 +0200

    add: octokit dep and successful collaborators read test

--- index.js
+++ index.js
@@ -1,32 +1,62 @@
-const { Client } = require('@notionhq/client');
+const { Client, LogLevel } = require('@notionhq/client');
+const { Octokit } = require('@octokit/core');
 const { config } = require('dotenv');
 
 config();
 
-const pageId = process.env.NOTION_PAGE_ID;
-const apiKey = process.env.NOTION_API_KEY;
+const notionPageId = process.env.NOTION_PAGE_ID;
+const notionApiKey = process.env.NOTION_API_KEY;
+const ghAuthToken = process.env.GH_AUTH_TOKEN;
 
-const notion = new Client({ auth: apiKey });
+const notion = new Client({ auth: notionApiKey, logLevel: LogLevel.DEBUG });
+const octokit = new Octokit({ auth: ghAuthToken });
 
 const columnName = 'GH Handle';
 
-const querryDatabase = async () => {
+async function querryNotionDatabase() {
   try {
-    const res = await notion.databases.query({
-      database_id: pageId,
-    });
-
-    console.log(
-      'RESULT: ',
-      res.results.map(
-        (prop) => prop.properties[columnName].rich_text[0].plain_text
-      )
-    );
+    // const res = await notion.databases.query({
+    //   database_id: notionPageId,
+    // });
+    // console.log(
+    //   'RESULT: ',
+    //   res.results.map(
+    //     (prop) => prop.properties[columnName].rich_text[0].plain_text
+    //   )
+    // );
   } catch (error) {
     console.log('ERROR: ', error);
   }
-};
+}
 
-querryDatabase();
+async function getRepoCollaborators() {
+  // await octokit.request('GET /repos/{owner}/{repo}/collaborators/{username}', {
+  //   owner: 'swor71',
+  //   repo: 'App',
+  //   username: 'USERNAME',
+  //   headers: {
+  //     'X-GitHub-Api-Version': '2022-11-28',
+  //   },
+  // });
 
-// const BASE_URL = 'https://api.notion.com';
+  try {
+    const res = await octokit.request(
+      'GET /repos/{owner}/{repo}/collaborators',
+      {
+        owner: 'swor71',
+        repo: 'App',
+        // headers: {
+        //   'X-GitHub-Api-Version': '2022-11-28'
+        // }
+      }
+    );
+
+    console.log('gh res', res);
+  } catch (error) {
+    console.log('ERROR: ', error);
+  }
+}
+
+getRepoCollaborators();
+
+// querryNotionDatabase();
